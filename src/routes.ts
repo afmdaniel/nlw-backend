03a4e3e53aaa24d673f46ef6747123da5050566e
afmdanielwin <danielfarias380@gmail.com>
2020-06-02 23:40:01 -0300

    Get items for collection

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express'
+import knex from './database/connection'
 
 const routes = Router()
 
-routes.get('/', (request, response) => {
-    return response.status(200).send({ message: 'Hello, World!'} )
+routes.get('/items', async (request, response) => {
+    const items = await knex('items_for_collection').select('*')
+    
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        }
+    })
+
+    return response.status(200).send(serializedItems)
 })
 
 export default routes
