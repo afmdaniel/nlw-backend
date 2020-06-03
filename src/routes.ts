51a7e887c589d9c7f8280640a8b69d8d4f6f0840
afmdanielwin <danielfarias380@gmail.com>
2020-06-03 00:51:44 -0300

    Register collection point

--- src/routes.ts
+++ src/routes.ts
@@ -4,16 +4,20 @@ import knex from './database/connection'
 const routes = Router()
 
 routes.get('/items', async (request, response) => {
-    const items = await knex('items_for_collection').select('*')
+    try{
+        const items = await knex('items_for_collection').select('*')
+        
+        const serializedItems = items.map(item => {
+            return {
+                ...item,
+                image_url: `http://localhost:3333/uploads/${item.image}`
+            }
+        })
     
-    const serializedItems = items.map(item => {
-        return {
-            ...item,
-            image_url: `http://localhost:3333/uploads/${item.image}`
-        }
-    })
-
-    return response.status(200).send(serializedItems)
+        return response.status(200).json(serializedItems)
+    } catch (err) {
+        return response.status(500).send(err)
+    }
 })
 
 routes.post('/points', async (request, response) => {
@@ -24,21 +28,39 @@ routes.post('/points', async (request, response) => {
         latitude,
         longitude,
         city,
-        uf
+        uf,
+        items
     } = request.body
 
-    knex('garbage_collection_points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf
-    })
+    const trx = await knex.transaction()
+    
+    try {
+        const [ point_id ] = await trx('garbage_collection_points').insert({
+            image: 'image-fake',
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf,
+        }).returning('id')
+    
+        const pointItems = items.map((item_id: number) => {
+            return {
+                item_id,
+                point_id
+            }
+        })
+    
+        await trx('items_points').insert(pointItems)
+        trx.commit()
+    } catch (err) {
+        trx.rollback()
+        return response.status(500).json(err)
+    }
 
-
+    return response.status(201).json({ success: true })
 })
 
 export default routes
