83b7c56c69f4d1e900abf9ad320624e8d26fc2f7
afmdanielwin <danielfarias380@gmail.com>
2020-06-05 20:18:48 -0300

    Set local ip address to have access from the mobile

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
             const serializedItems = items.map(item => {
                 return {
                     ...item,
-                    image_url: `http://localhost:3333/uploads/${item.image}`
+                    image_url: `http://192.168.0.17:3333/uploads/${item.image}`
                 }
             })
         
